bac614c2e60aa92198fe573ab4a77e13272c6bfd
merthaa <mntri121@gmail.com>
2025-12-06 23:52:18 +0700

    update to dynamic title

--- src/component/AnimeList/index.js
+++ src/component/AnimeList/index.js
@@ -1,12 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const AnimeList = ({title, images, id}) => {
+const AnimeList = ({ api }) => {
     return (
-        <Link href={`/${id}`} className="cursor-pointer">
-            <Image src={images} alt="..." width={350} height={750} className="w-full max-h-200 object-fill" unoptimized/>
-            <h3 className="font-bold md:text-2xl text-xl p-3">{title}</h3>
-        </Link>
+        <div className="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
+            {api.data.map((anime) => {
+                return (
+                    <Link key={anime.mal_id} href={`/${anime.mal_id}`} className="cursor-pointer">
+                        <Image src={anime.images.webp.image_url} alt="..." width={350} height={750} className="w-full max-h-200 object-fill" unoptimized />
+                        <h3 className="font-bold md:text-2xl text-xl p-3">{anime.title}</h3>
+                    </Link>
+                )
+            })}
+
+        </div>
+
     )
 }
 
