3dc6582fbaddb85e2452f348d7582819bb3226b6
merthaa <mntri121@gmail.com>
2025-12-06 23:54:44 +0700

    update source component navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "../component/utilities/Navbar";
+import Navbar from "../component/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
