2f720e8266898c2191408883202d497cd2bcf738
merthaa <mntri121@gmail.com>
2025-12-06 23:51:02 +0700

    separate input from index component

--- src/component/Navbar/index.js
+++ src/component/Navbar/index.js
@@ -1,12 +1,14 @@
 import Link from "next/link";
+import InputSearch from "./inputSearch";
+
 
 const Navbar = () => {
     return (
-        <header className="bg-gray-700 py-6 px-4 flex md:flex-row flex-col justify-between gap-2 items-center">
-            <div className="">
+        <header className="bg-gray-700 py-6 px-4">
+            <div className="flex md:flex-row flex-col justify-between gap-2 items-center">
                 <Link href="/" className="font-bold text-white text-4xl">MERTANIMELIST</Link>
+                <InputSearch />
             </div>
-            <input placeholder="Cari anime.." className="bg-white border-2 rounded-md border-white"  />
         </header>
     )
 }
